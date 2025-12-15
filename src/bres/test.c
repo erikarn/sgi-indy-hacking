bcb7c49bf1315ebff9df012a87674df1d459291d
Adrian Chadd <adrian@FreeBSD.org>
2025-12-14 23:31:36 -0800

    bres: some fixes, start handling arbitrary triangles
    * the y iterator in the negative case wasn't iterating from the correct
      y value, it was starting from the lower value and then contiuing
      to decrement.  so, fix that.
    
    * add in initial, barely tested triangle point -> one or two triangle
      decomposition.
    
    * point2d, point3d bits, for testing
    
    Next up is cleaning this all up to populate a scan list that I can
    draw, likely first in SDL so I can see it in action!

--- src/bres/test.c
+++ src/bres/test.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <sys/types.h>
 
+struct point2d {
+	int x, y;
+};
+
+struct point3d {
+	int x, y, z;
+};
 
 /*
  * Experiments rendering a flat top or bottom triangle
@@ -36,16 +44,13 @@ test_triangle_1(int x1, int y1, int x2l, int x2r, int y2)
 	int X_right = x1;
 	int yi;
 	const int y_sign = (y2 < y1) ? -1 : 1;
-	const int yi1 = (y_sign == 1) ? y1 : y2;
-	const int yi2 = (y_sign == 1) ? y2 : y1;
-	const int yd = yi2 - yi1;
-	int yc = (yi2 - yi1);
-	int E_left = -(yi2 - yi1) - (x2l - x1);
-	int E_right = -(yi2 - yi1) - (x2r - x1);
+	int yc = (y2 - y1) * y_sign;
+	int E_left = -((y2 - y1) * y_sign) - (x2l - x1);
+	int E_right = -((y2 - y1) * y_sign) - (x2r - x1);
 
 	printf("%s:  (%d,%d) -> (%d,%d), (%d,%d) \n",
 	    __func__, x1, y1, x2l, y2, x2r, y2);
-	//printf("%s: xl_sign=%d, xr_sign=%d\n", __func__, xl_sign, xr_sign);
+	printf("%s: xl_sign=%d, xr_sign=%d, y_sign=%d\n", __func__, xl_sign, xr_sign, y_sign);
 
 	/*
 	 * Handle y1 == y2; but ideally the caller would not bother and
@@ -56,18 +61,19 @@ test_triangle_1(int x1, int y1, int x2l, int x2r, int y2)
 		return;
 	}
 
-	for (yi = yi1; yc >= 0; yc--, yi += y_sign) {
-		//printf("%s:  e_left=%d, e_right=%d\n", __func__, E_left, E_right);
+	/* TODO: is yc >= 0 correct? or yc > 0, and we need to add the final line/point? */
+	for (yi = y1; yc >= 0; yc--, yi += y_sign) {
+//		printf("%s:  e_left=%d, e_right=%d\n", __func__, E_left, E_right);
 		/* left hand iterator */
 		while (E_left >= 0) {
 			X_left += xl_sign;
-			E_left -= 2* (yi2 - yi1);
+			E_left -= 2* ((y2 - y1) * y_sign);
 		}
 
 		/* right hand iterator */
 		while (E_right >= 0) {
 			X_right += xr_sign;
-			E_right -= 2* (yi2 - yi1);
+			E_right -= 2* ((y2 - y1) * y_sign);
 		}
 
 		printf("%s:  (%d, %d),%d\n", __func__, X_left, X_right, yi);
@@ -77,11 +83,91 @@ test_triangle_1(int x1, int y1, int x2l, int x2r, int y2)
 	}
 }
 
+/**
+ * Given a triangle (x1,y1), (x2,y2), (x3,y3), generate the
+ * scan list.
+ */
+void
+test_triangle(struct point2d *plist)
+{
+	struct point2d mp;
+	bool valid_mp;
+
+	int a = 0, b = 1, c = 2, t;
+
+	/* Assume that plist[0,1,2] contain the triangle points */
+
+	/*
+	 * Step one - sort them by y, since we need to find the
+	 * midpoint that may become two triangles.
+	 */
+	if (plist[a].y > plist[b].y) {
+		t = b; a = b; b = a;
+	}
+	if (plist[b].y > plist[c].y) {
+		t = c; a = c; c = b;
+	}
+
+	printf("%s: points: (%d, %d, %d) a=(%d,%d), b=(%d,%d), c=(%d,%d)\n",
+	    __func__, a, b, c,
+	    plist[a].x, plist[a].y,
+	    plist[b].x, plist[b].y,
+	    plist[c].x, plist[c].y);
+
+	if (plist[b].y == plist[c].y) {
+		/* Flat bottom triangle */
+		/* plist[a] is the origin point, going to b, c */
+		test_triangle_1(plist[a].x, plist[a].y,
+		    plist[b].x, plist[c].x, plist[c].y);
+	} else if (plist[a].y == plist[b].y) {
+		/* Flat top triangle */
+		/* plist[c] is the origin point, going to a, b */
+		test_triangle_1(plist[c].x, plist[c].y,
+		    plist[a].x, plist[b].x, plist[b].y);
+	} else {
+		int dx, dy, by;
+
+		/* Split */
+		/*
+		 * Now for the midpoint (b), we need to create another point
+		 * (mp) which bisects the long axis - which here should be
+		 * (a -> c.) (mp) will have an x value between (a.x) and (c.x)
+		 * at (b.y).
+		 */
+		mp.y = plist[b].y;
+		dy = (plist[c].y - plist[a].y) * 1024;
+		dx = (plist[c].x - plist[a].x) * 1024;
+		by = (plist[b].y - plist[a].y) * 1024;
+		mp.x = plist[a].x + ((by / dy) * dx) / 1024;
+
+		/* Flat bottom - a, b, mp; mp.y == b.y */
+		test_triangle_1(plist[a].x, plist[a].y,
+		    plist[b].x, mp.x, mp.y);
+
+		/* Flat top - b, mp, c, mp.y == c.y */
+		test_triangle_1(plist[c].x, plist[c].y,
+		    mp.x, plist[b].x, plist[b].y);
+	}
+}
+
+void
+test_triangle_xy(int x1, int y1, int x2, int y2, int x3, int y3)
+{
+	struct point2d p[3];
+
+	p[0].x = x1; p[0].y = y1;
+	p[1].x = x2; p[1].y = y2;
+	p[2].x = x3; p[2].y = y3;
+
+	test_triangle(p);
+}
+
 int
 main(int argc, const char *argv[])
 {
 	             /* x1, y1, x2l, x2r,  y2 */
 
+#if 1
 	/* y2 > y1 */
 	test_triangle_1(40, 20, 5,  60,  30);
 	test_triangle_1(40, 20, 5,  10,  30);
@@ -106,5 +192,18 @@ main(int argc, const char *argv[])
 	test_triangle_1(40, 30, 40,  60,  30);
 	test_triangle_1(40, 30, 30,  40,  30);
 
+#endif
+
+	/*
+	 * TODO: many more test arbitrary triangles, and maybe
+	 * a unit test or two.
+	 */
+	test_triangle_xy(50, 50, 30, 70, 40, 80);
+
+	/* TODO: these routines need to populate a scan list of (x1, x2, y) for rendering! */
+
 	exit(0);
 }
+
+
+
