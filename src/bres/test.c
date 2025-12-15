ead4871a25f6cd4c86ca82d9f39c233d61a22f9e
Adrian Chadd <adrian@FreeBSD.org>
2025-12-14 17:31:38 -0800

    bres: handle x2l and x2r being on either side of x1
    This should handle all of the triangle cases with a starting point
    at x1,y1?

--- src/bres/test.c
+++ src/bres/test.c
@@ -18,37 +18,46 @@
 void
 test_triangle_1(int x1, int y1, int x2l, int x2r, int y2)
 {
+	int xl_sign = (x2l < x1) ? -1 : 1;
+	int xr_sign = (x2r < x1) ? -1 : 1;
 	int E_left = -(y2 - y1) - (x2l - x1);
-	int E_right = (y2 - y1) - (x2r - x1);
+	int E_right = -(y2 - y1) - (x2r - x1);
 	int X_left = x1;
 	int X_right = x1;
 	int yi;
 
-	printf("%s:  %d,%d\n", __func__, x1, y1);
+	printf("%s:  (%d,%d) -> (%d,%d), (%d,%d) \n",
+	    __func__, x1, y1, x2l, y2, x2r, y2);
+	//printf("%s: xl_sign=%d, xr_sign=%d\n", __func__, xl_sign, xr_sign);
 
 	for (yi = y1; yi < y2; yi++) {
+		//printf("%s:  e_left=%d, e_right=%d\n", __func__, E_left, E_right);
 		/* left hand iterator */
 		while (E_left >= 0) {
-			X_left--;
+			X_left += xl_sign;
 			E_left -= 2* (y2 - y1);
 		}
 
 		/* right hand iterator */
-		while (E_right < 0) {
-			X_right++;
-			E_right += 2* (y2 - y1);
+		while (E_right >= 0) {
+			X_right += xr_sign;
+			E_right -= 2* (y2 - y1);
 		}
 
 		printf("%s:  (%d, %d),%d\n", __func__, X_left, X_right, yi);
 
-		E_left -= 2 * (x2l - x1);
-		E_right -= 2 * (x2r - x1);
+		E_left -= 2 * -xl_sign * (x2l - x1);
+		E_right -= 2 * -xr_sign * (x2r - x1);
 	}
 }
 
 int
 main(int argc, const char *argv[])
 {
-	test_triangle_1(20, 20, 5, 25, 30);
+	             /* x1, y1, x2l, x2r,  y2 */
+	test_triangle_1(40, 20, 5,  60,  30);
+	test_triangle_1(40, 20, 5,  10,  30);
+	test_triangle_1(40, 20, 50,  60,  30);
+	test_triangle_1(40, 20, 30,  60,  30);
 	exit(0);
 }
