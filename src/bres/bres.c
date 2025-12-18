c645c846323cd135e736a18790befca2eb8f08c2
Adrian Chadd <adrian@FreeBSD.org>
2025-12-17 23:17:27 -0800

    bres: fix the midpoint line calculation; add debugging
    * oops, midpoint line fixed point math was not quite right, leading the
      delta x term 0
    
    * add debugging and put it all behind ifdefs

--- src/bres/bres.c
+++ src/bres/bres.c
@@ -8,6 +8,10 @@
 #include "scanline.h"
 #include "bres.h"
 
+//#define DEBUG_TRIANGLE
+#define DRAW_TRIANGLE
+//#define PRINT_TRIANGLE_SETUP
+
 /*
  * Experiments rendering a flat top or bottom triangle
  * from (x1,y1) -> (x2l, y2) / (x2r, y2)
@@ -45,7 +49,7 @@ bres_triangle_flat(struct scanline_list *slist, int x1, int y1, int x2l,
 	int E_left = -((y2 - y1) * y_sign) - (x2l - x1);
 	int E_right = -((y2 - y1) * y_sign) - (x2r - x1);
 
-#if 0
+#ifdef DEBUG_TRIANGLE
 	printf("%s:  (%d,%d) -> (%d,%d), (%d,%d) \n",
 	    __func__, x1, y1, x2l, y2, x2r, y2);
 	printf("%s: xl_sign=%d, xr_sign=%d, y_sign=%d\n", __func__, xl_sign, xr_sign, y_sign);
@@ -62,7 +66,9 @@ bres_triangle_flat(struct scanline_list *slist, int x1, int y1, int x2l,
 
 	/* TODO: is yc >= 0 correct? or yc > 0, and we need to add the final line/point? */
 	for (yi = y1; yc >= 0; yc--, yi += y_sign) {
-//		printf("%s:  e_left=%d, e_right=%d\n", __func__, E_left, E_right);
+#ifdef DEBUG_TRIANGLE
+		printf("%s:  e_left=%d, e_right=%d\n", __func__, E_left, E_right);
+#endif
 		/* left hand iterator */
 		while (E_left >= 0) {
 			X_left += xl_sign;
@@ -74,12 +80,17 @@ bres_triangle_flat(struct scanline_list *slist, int x1, int y1, int x2l,
 			X_right += xr_sign;
 			E_right -= 2* ((y2 - y1) * y_sign);
 		}
+#ifdef DEBUG_TRIANGLE
+#endif
 
 		scanline_list_push(slist, X_left, X_right, yi);
 
 		E_left -= 2 * -xl_sign * (x2l - x1);
 		E_right -= 2 * -xr_sign * (x2r - x1);
 	}
+#ifdef DEBUG_TRIANGLE
+	printf("%s: done\n", __func__);
+#endif
 }
 
 /**
@@ -107,7 +118,7 @@ bres_triangle(struct point2d *plist, struct scanline_list **slist)
 		t = c; a = c; c = b;
 	}
 
-#if 0
+#ifdef PRINT_TRIANGLE_SETUP
 	printf("%s: points: (%d, %d, %d) a=(%d,%d), b=(%d,%d), c=(%d,%d)\n",
 	    __func__, a, b, c,
 	    plist[a].x, plist[a].y,
@@ -121,15 +132,19 @@ bres_triangle(struct point2d *plist, struct scanline_list **slist)
 	if (plist[b].y == plist[c].y) {
 		/* Flat bottom triangle */
 		/* plist[a] is the origin point, going to b, c */
+#ifdef DRAW_TRIANGLE
 		bres_triangle_flat(*slist, plist[a].x, plist[a].y,
 		    plist[b].x, plist[c].x, plist[c].y);
+#endif
 	} else if (plist[a].y == plist[b].y) {
 		/* Flat top triangle */
 		/* plist[c] is the origin point, going to a, b */
+#ifdef DRAW_TRIANGLE
 		bres_triangle_flat(*slist, plist[c].x, plist[c].y,
 		    plist[a].x, plist[b].x, plist[b].y);
+#endif
 	} else {
-		int dx, dy, by;
+		int dx, dy, by, xinc;
 
 		/* Split */
 		/*
@@ -137,20 +152,35 @@ bres_triangle(struct point2d *plist, struct scanline_list **slist)
 		 * (mp) which bisects the long axis - which here should be
 		 * (a -> c.) (mp) will have an x value between (a.x) and (c.x)
 		 * at (b.y).
+		 *
+		 * The formula is mp.x = a.x + ((b.y-a.y)/(c.y-a.y))*(c.x-a.x)
+		 * To avoid using floats we do a bit of fixed point math.
 		 */
 		mp.y = plist[b].y;
-		dy = (plist[c].y - plist[a].y) * 1024;
-		dx = (plist[c].x - plist[a].x) * 1024;
+		dy = (plist[c].y - plist[a].y);
+		dx = (plist[c].x - plist[a].x);
 		by = (plist[b].y - plist[a].y) * 1024;
-		mp.x = plist[a].x + ((by / dy) * dx) / 1024;
+		xinc = (((by / dy) * dx)) / 1024;
+		mp.x = plist[a].x + xinc;
 
+
+#ifdef PRINT_TRIANGLE_SETUP
+	printf("%s: [a].x=%d, xinc=%d\n", __func__, plist[a].x, xinc);
+	printf("%s: mp.x=%d, mp.y=%d\n", __func__, mp.x, mp.y);
+	printf("%s: dy=%d, dx=%d, by=%d\n", __func__, dy, dx, by);
+#endif
+
+#ifdef DRAW_TRIANGLE
 		/* Flat bottom - a, b, mp; mp.y == b.y */
 		bres_triangle_flat(*slist, plist[a].x, plist[a].y,
 		    plist[b].x, mp.x, mp.y);
+#endif
 
+#ifdef DRAW_TRIANGLE
 		/* Flat top - b, mp, c, mp.y == c.y */
 		bres_triangle_flat(*slist, plist[c].x, plist[c].y,
 		    plist[b].x, mp.x, plist[b].y);
+#endif
 	}
 }
 
