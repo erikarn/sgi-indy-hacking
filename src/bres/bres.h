8109f2dde894fd9f09bedf968a040cc253265714
Adrian Chadd <adrian@FreeBSD.org>
2025-12-17 23:19:50 -0800

    bres: export the triangle top/bottom drawing, oops

--- src/bres/bres.h
+++ src/bres/bres.h
@@ -1,6 +1,8 @@
 #ifndef	__BRES_H__
 #define	__BRES_H__
 
+extern	void bres_triangle_flat(struct scanline_list *slist, int x1, int y1,
+	    int x2l, int x2r, int y2);
 extern	void bres_triangle_xy(int x1, int y1, int x2, int y2, int x3, int y3,
 	    struct scanline_list **slist);
 
